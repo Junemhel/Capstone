ed28475986ef982a79a3dd438610d227cfb81831
HerahFatima <90399700+HerahFatima@users.noreply.github.com>
2022-11-03 15:31:47 +0800

    Panic

--- Etanong Github Copy/App/urls.py
+++ Etanong Github Copy/App/urls.py
@@ -23,16 +23,16 @@ urlpatterns = [
     path('randompicker', views.randompickerView.as_view(), name="randompicker_view"),
     path('teacherdashboard', views.teacherdashboardView.as_view(), name="teacherdashboard_view"),
     path('studentdashboard', views.studentdashboardView.as_view(), name="studentdashboard_view"),
-    path('teachers/', include(([
-        path('', views.QuizListView.as_view(), name='quiz_change_list'),
-        path('quiz/add/', views.QuizCreateView.as_view(), name='quiz_add'),
-        path('quiz/<int:pk>/', views.QuizUpdateView.as_view(), name='quiz_change'),
-        path('quiz/<int:pk>/delete/', views.QuizDeleteView.as_view(), name='quiz_delete'),
-        path('quiz/<int:pk>/results/', views.QuizResultsView.as_view(), name='quiz_results'),
-        path('quiz/<int:pk>/question/add/', views.question_add, name='question_add'),
-        path('quiz/<int:quiz_pk>/question/<int:question_pk>/', views.question_change, name='question_change'),
-        path('quiz/<int:quiz_pk>/question/<int:question_pk>/delete/', views.QuestionDeleteView.as_view(), name='question_delete'),
-    ], 'classroom'), namespace='teachers')),
+    # path('teachers/', include(([
+    #     path('', views.QuizListView.as_view(), name='quiz_change_list'),
+    #     path('quiz/add/', views.QuizCreateView.as_view(), name='quiz_add'),
+    #     path('quiz/<int:pk>/', views.QuizUpdateView.as_view(), name='quiz_change'),
+    #     path('quiz/<int:pk>/delete/', views.QuizDeleteView.as_view(), name='quiz_delete'),
+    #     path('quiz/<int:pk>/results/', views.QuizResultsView.as_view(), name='quiz_results'),
+    #     path('quiz/<int:pk>/question/add/', views.question_add, name='question_add'),
+    #     path('quiz/<int:quiz_pk>/question/<int:question_pk>/', views.question_change, name='question_change'),
+    #     path('quiz/<int:quiz_pk>/question/<int:question_pk>/delete/', views.QuestionDeleteView.as_view(), name='question_delete'),
+    # ], 'classroom'), namespace='teachers')),
 ]    
     
    

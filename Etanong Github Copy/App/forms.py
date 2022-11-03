a0a00e225734a3f23d6914c438c954b8251ed6a2
Junemhel <junemhelrabadon@gmail.com>
2022-11-03 14:12:22 +0800

    Merging Quiz Template Files (June)

--- Etanong Github Copy/App/forms.py
+++ Etanong Github Copy/App/forms.py
@@ -1,24 +1,85 @@
 from django import forms
-from .models import *
 from django.contrib.auth.forms import UserCreationForm
-from django.core.exceptions import ValidationError
+from django.db import transaction
+from django.forms.utils import ValidationError
 
-class ClassroomForm(forms.ModelForm):
-	class Meta:
-		model = Classroom
-		fields= '__all__'
-
-class QuizForm(forms.ModelForm):
-	class Meta:
-		model = Quiz
-		fields= '__all__'
+from App.models import (Answer, Question, Student, StudentAnswer,
+                              Subject, User)
 
 
-class UserForm(UserCreationForm):
-    firstname = forms.CharField(max_length= 100)
-    lastname = forms.CharField(max_length= 100)
-    phone_number = forms.CharField(max_length=11)
-    email = forms.EmailField()
-    class Meta:
+class TeacherSignUpForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
         model = User
-        fields =  ('firstname', 'lastname', 'phone_number', 'email', 'username', 'password1', 'password2' )
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_teacher = True
+        if commit:
+            user.save()
+        return user
+
+
+class StudentSignUpForm(UserCreationForm):
+    interests = forms.ModelMultipleChoiceField(
+        queryset=Subject.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=True
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_student = True
+        user.save()
+        student = Student.objects.create(user=user)
+        student.interests.add(*self.cleaned_data.get('interests'))
+        return user
+
+
+class StudentInterestsForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ('interests', )
+        widgets = {
+            'interests': forms.CheckboxSelectMultiple
+        }
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ('text', )
+
+
+class BaseAnswerInlineFormSet(forms.BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+
+        has_one_correct_answer = False
+        for form in self.forms:
+            if not form.cleaned_data.get('DELETE', False):
+                if form.cleaned_data.get('is_correct', False):
+                    has_one_correct_answer = True
+                    break
+        if not has_one_correct_answer:
+            raise ValidationError('Mark at least one answer as correct.', code='no_correct_answer')
+
+
+class TakeQuizForm(forms.ModelForm):
+    answer = forms.ModelChoiceField(
+        queryset=Answer.objects.none(),
+        widget=forms.RadioSelect(),
+        required=True,
+        empty_label=None)
+
+    class Meta:
+        model = StudentAnswer
+        fields = ('answer', )
+
+    def __init__(self, *args, **kwargs):
+        question = kwargs.pop('question')
+        super().__init__(*args, **kwargs)
+        self.fields['answer'].queryset = question.answers.order_by('text')
